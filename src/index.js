f1db8098acec6e2881496e3a8548b7173adf3d39
Horoshaya <43165019+Horoshaya@users.noreply.github.com>
2018-09-28 15:31:20 +0300

    7

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 // DO WHATEVER YOU WANT HERE
-
+let count = 0;
+let asyncCount = 0;
 const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createNotEnumerableProperty = () => { return Symbol()}; //propertyName = value
+const createProtoMagicObject = () => { return Function};
+const incrementor = () => {count++; return incrementor};
+incrementor.valueOf = () => count;
 
+const asyncIncrementor = () => {return ++asyncCount};
+const createIncrementer = () => {
+    let count = 0;
+    function* next() {while (true) yield ++count}
+    return next()
+};
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (value) => {     
+    return new Promise((a) => {setTimeout(() => {a(value);}, 1000)});
+};
+
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
